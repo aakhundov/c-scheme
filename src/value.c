23e7e5298b5b023e78fec035ddde593cfead5074
Adnan Akhundov <adnan.akhundov@argmax.ai>
2022-08-15 22:29:50 +0200

    Extend memory management of values.

--- src/value.c
+++ src/value.c
@@ -7,91 +7,138 @@
 
 #include "str.h"
 
-value* value_new_number(double number) {
-    value* v = malloc(sizeof(value));
-
+void value_init_number(value* v, double number) {
     v->type = VALUE_NUMBER;
     v->number = number;
+}
+
+void value_init_symbol(value* v, char* symbol) {
+    v->type = VALUE_SYMBOL;
+    v->symbol = malloc(strlen(symbol) + 1);
+    strcpy(v->symbol, symbol);
+}
+
+void value_init_string(value* v, char* string) {
+    v->type = VALUE_STRING;
+    v->symbol = malloc(strlen(string) + 1);
+    strcpy(v->symbol, string);
+}
+
+static void value_init_symbol_from_args(value* v, char* format, va_list args) {
+    char buffer[1024];
+    vsnprintf(buffer, sizeof(buffer), format, args);
+    value_init_symbol(v, buffer);
+}
+
+void value_init_error_from_args(value* v, char* error, va_list args) {
+    value_init_symbol_from_args(v, error, args);
+    v->type = VALUE_ERROR;
+}
+
+void value_init_error(value* v, char* error, ...) {
+    va_list args;
+    va_start(args, error);
+    value_init_error_from_args(v, error, args);
+    va_end(args);
+}
+
+void value_init_info_from_args(value* v, char* info, va_list args) {
+    value_init_symbol_from_args(v, info, args);
+    v->type = VALUE_INFO;
+}
+
+void value_init_info(value* v, char* info, ...) {
+    va_list args;
+    va_start(args, info);
+    value_init_info_from_args(v, info, args);
+    va_end(args);
+}
+
+void value_init_pair(value* v, value* car, value* cdr) {
+    v->type = VALUE_PAIR;
+    v->car = car;
+    v->cdr = cdr;
+}
+
+void value_init_null_pair(value* v) {
+    value_init_pair(v, NULL, NULL);
+}
+
+value* value_new_number(double number) {
+    value* v = malloc(sizeof(value));
+    value_init_number(v, number);
 
     return v;
 }
 
 value* value_new_symbol(char* symbol) {
     value* v = malloc(sizeof(value));
-
-    v->type = VALUE_SYMBOL;
-    v->symbol = malloc(strlen(symbol) + 1);
-    strcpy(v->symbol, symbol);
-
-    return v;
-}
-
-static value* value_new_symbol_from_args(char* format, va_list args) {
-    char buffer[1024];
-    vsnprintf(buffer, sizeof(buffer), format, args);
-
-    return value_new_symbol(buffer);
-}
-
-value* value_new_error_from_args(char* error, va_list args) {
-    value* v = value_new_symbol_from_args(error, args);
-    v->type = VALUE_ERROR;
-
-    return v;
-}
-
-value* value_new_error(char* error, ...) {
-    va_list args;
-    va_start(args, error);
-    value* v = value_new_error_from_args(error, args);
-    va_end(args);
-
-    return v;
-}
-
-value* value_new_info_from_args(char* info, va_list args) {
-    value* v = value_new_symbol_from_args(info, args);
-    v->type = VALUE_INFO;
-
-    return v;
-}
-
-value* value_new_info(char* info, ...) {
-    va_list args;
-    va_start(args, info);
-    value* v = value_new_info_from_args(info, args);
-    va_end(args);
+    value_init_symbol(v, symbol);
 
     return v;
 }
 
 value* value_new_string(char* string) {
     value* v = malloc(sizeof(value));
+    value_init_string(v, string);
 
-    v->type = VALUE_STRING;
-    v->symbol = malloc(strlen(string) + 1);
-    strcpy(v->symbol, string);
+    return v;
+}
+
+value* value_new_error(char* error, ...) {
+    value* v = malloc(sizeof(value));
+
+    va_list args;
+    va_start(args, error);
+    value_init_error_from_args(v, error, args);
+    va_end(args);
+
+    return v;
+}
+
+value* value_new_error_from_args(char* error, va_list args) {
+    value* v = malloc(sizeof(value));
+    value_init_error_from_args(v, error, args);
+
+    return v;
+}
+
+value* value_new_info(char* info, ...) {
+    value* v = malloc(sizeof(value));
+
+    va_list args;
+    va_start(args, info);
+    value_init_info_from_args(v, info, args);
+    va_end(args);
+
+    return v;
+}
+
+value* value_new_info_from_args(char* info, va_list args) {
+    value* v = malloc(sizeof(value));
+    value_init_info_from_args(v, info, args);
 
     return v;
 }
 
 value* value_new_pair(value* car, value* cdr) {
     value* v = malloc(sizeof(value));
-
-    v->type = VALUE_PAIR;
-    v->car = car;
-    v->cdr = cdr;
+    value_init_pair(v, car, cdr);
 
     return v;
 }
 
 value* value_new_null_pair() {
-    return value_new_pair(NULL, NULL);
+    value* v = malloc(sizeof(value));
+    value_init_null_pair(v);
+
+    return v;
 }
 
-void value_dispose(value* v) {
+void value_cleanup(value* v) {
     switch (v->type) {
         case VALUE_NUMBER:
+        case VALUE_PAIR:
             break;
         case VALUE_SYMBOL:
         case VALUE_STRING:
@@ -99,14 +146,19 @@ void value_dispose(value* v) {
         case VALUE_INFO:
             free(v->symbol);
             break;
-        case VALUE_PAIR:
-            if (v->car != NULL) {
-                value_dispose(v->car);
-            }
-            if (v->cdr != NULL) {
-                value_dispose(v->cdr);
-            }
-            break;
+    }
+}
+
+void value_dispose(value* v) {
+    if (v->type == VALUE_PAIR) {
+        if (v->car != NULL) {
+            value_dispose(v->car);
+        }
+        if (v->cdr != NULL) {
+            value_dispose(v->cdr);
+        }
+    } else {
+        value_cleanup(v);
     }
 
     free(v);
@@ -180,4 +232,39 @@ value* value_add_child(value* parent, value* child) {
     parent->cdr = value_new_null_pair();
 
     return parent->cdr;
+}
+
+void value_copy(value* dest, value* source) {
+    switch (source->type) {
+        case VALUE_NUMBER:
+            value_init_number(dest, source->number);
+            break;
+        case VALUE_SYMBOL:
+            value_init_symbol(dest, source->symbol);
+            break;
+        case VALUE_STRING:
+            value_init_string(dest, source->symbol);
+            break;
+        case VALUE_ERROR:
+            value_init_error(dest, source->symbol);
+            break;
+        case VALUE_INFO:
+            value_init_info(dest, source->symbol);
+            break;
+        case VALUE_PAIR:
+            value_init_pair(dest, source->car, source->cdr);
+            break;
+    }
+}
+
+value* value_clone(value* source) {
+    value* dest = malloc(sizeof(value));
+    value_copy(dest, source);
+
+    if (dest->type == VALUE_PAIR) {
+        dest->car = value_clone(dest->car);
+        dest->cdr = value_clone(dest->cdr);
+    }
+
+    return dest;
 }
--- src/value.h
+++ src/value.h
@@ -16,14 +16,24 @@ typedef struct value value;
 
 struct value {
     value_type type;
-
     double number;
     char* symbol;
-
     value* car;
     value* cdr;
 };
 
+// without malloc
+void value_init_number(value* v, double number);
+void value_init_symbol(value* v, char* symbol);
+void value_init_string(value* v, char* string);
+void value_init_error(value* v, char* error, ...);
+void value_init_error_from_args(value* v, char* error, va_list args);
+void value_init_info(value* v, char* info, ...);
+void value_init_info_from_args(value* v, char* info, va_list args);
+void value_init_pair(value* v, value* car, value* cdr);
+void value_init_null_pair(value* v);
+
+// with malloc
 value* value_new_number(double number);
 value* value_new_symbol(char* symbol);
 value* value_new_string(char* string);
@@ -34,9 +44,14 @@ value* value_new_info_from_args(char* info, va_list args);
 value* value_new_pair(value* car, value* cdr);
 value* value_new_null_pair();
 
-void value_dispose(value* v);
+void value_cleanup(value* v);  // without free
+void value_dispose(value* v);  // with free
+
 int value_is_null_pair(value* v);
 int value_to_str(value* v, char* buffer);
 value* value_add_child(value* parent, value* child);
+
+void value_copy(value* dest, value* source);  // without malloc
+value* value_clone(value* source);            // with malloc
 
 #endif  // VALUE_H_
