d1a885f50ad8d39516572a184c64f485ee59cfea
Adnan Akhundov <adnan.akhundov@argmax.ai>
2022-08-16 23:16:35 +0200

    Fix function names.

--- src/pool.h
+++ src/pool.h
@@ -21,15 +21,15 @@ void pool_register_root(pool* p, value* root);
 void pool_unregister_root(pool* p, value* root);
 void pool_collect_garbage(pool* p);
 
-value* pool_make_number(pool* p, double number);
-value* pool_make_symbol(pool* p, char* symbol);
-value* pool_make_string(pool* p, char* string);
-value* pool_make_error(pool* p, char* error, ...);
-value* pool_make_error_from_args(pool* p, char* error, va_list args);
-value* pool_make_info(pool* p, char* info, ...);
-value* pool_make_info_from_args(pool* p, char* info, va_list args);
-value* pool_make_pair(pool* p, value* car, value* cdr);
-value* pool_make_null_pair(pool* p);
+value* pool_new_number(pool* p, double number);
+value* pool_new_symbol(pool* p, char* symbol);
+value* pool_new_string(pool* p, char* string);
+value* pool_new_error(pool* p, char* error, ...);
+value* pool_new_error_from_args(pool* p, char* error, va_list args);
+value* pool_new_info(pool* p, char* info, ...);
+value* pool_new_info_from_args(pool* p, char* info, va_list args);
+value* pool_new_pair(pool* p, value* car, value* cdr);
+value* pool_new_null_pair(pool* p);
 
 value* pool_import(pool* p, value* source);
 
